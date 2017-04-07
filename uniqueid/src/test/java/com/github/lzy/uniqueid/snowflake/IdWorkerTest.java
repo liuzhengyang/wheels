b7c1151e2c0ea63e0961c7015949e62542a5e7cb
liuzhengyang <liuzhengyang@meituan.com>
2017-04-07 16:50:12 +0800

    Add UnitTest

--- uniqueid/src/test/java/com/github/lzy/uniqueid/snowflake/IdWorkerTest.java
+++ uniqueid/src/test/java/com/github/lzy/uniqueid/snowflake/IdWorkerTest.java
@@ -1,8 +1,11 @@
 package com.github.lzy.uniqueid.snowflake;
 
 
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -19,14 +22,18 @@ public class IdWorkerTest {
 	public void getId() throws InterruptedException {
 		IdWorker idWorker = new IdWorker(1, 1);
 
+		Set<Long> idSets = new ConcurrentSkipListSet<>();
 		ExecutorService executorService = Executors.newFixedThreadPool(50);
-		for (int i = 0; i < 100000; i++) {
+		int totalSize = 100000;
+		for (int i = 0; i < totalSize; i++) {
 			executorService.submit(() -> {
 				long unique_id_test = idWorker.getId("unique_id_test");
+				idSets.add(unique_id_test);
 			});
 		}
 		executorService.shutdown();
 		executorService.awaitTermination(1, TimeUnit.HOURS);
+		Assert.assertEquals(idSets.size(), totalSize);
 	}
 
 }
